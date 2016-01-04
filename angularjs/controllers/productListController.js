83756c9cb2fa363574ad18eefb4100e3be36c01c
Ramkrishna M <vu2rmk@gmail.com>
2016-01-05 00:28:29 +0530

    Added Controllers, custom filters and edited app.html

---
+++ angularjs/controllers/productListController.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../../Scripts/angular.js" />
+
+/// <reference path="../ngmodules/sportsStoreModule.js" />
+
+app.controller('productListCtrl', ['$scope', '$filter', 'productListActiveClass', 'productListPageCount', function ($scope, $filter, productListActiveClass, productListPageCount) {
+    var selectedCategory = null;
+
+    $scope.selectedPage = 1;
+    $scope.pageSize = productListPageCount;
+
+    $scope.selectCategory = function (newCategory) {
+        //console.log(newCategory);
+        selectedCategory = newCategory;
+        $scope.selectedPage = 1;
+    }
+
+    $scope.selectPage = function (newPage) {
+        $scope.selectedPage = newPage;
+    }
+
+    $scope.categoryFilterFn = function (product) {
+        //console.log(product);
+        //console.log(selectedCategory);
+        //console.log(selectedCategory == null || product.category == selectedCategory);
+        return selectedCategory == null || product.category == selectedCategory;
+    }
+
+    $scope.getCategoryClass = function (category) {
+        return selectedCategory == category ? productListActiveClass : "";
+    }
+
+    $scope.getPageClass = function (page) {
+        return $scope.selectedPage == page ? productListActiveClass : "";
+    }
+
+}]);
---
+++ angularjs/ngmodules/sportsStoreModule.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../../Scripts/angular.js" />
+
+var app = angular.module('sportsStore', ['customFilter']);
+
+app.constant('productListActiveClass', 'btn-primary');
+app.constant('productListPageCount', 3);
+app.constant('dataUrl', 'productserror');
+
+
